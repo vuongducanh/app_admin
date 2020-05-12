3c50d2adf3b46cee5a3d5e61f0f6db028a69d799
anh.vuong <anh.vuong@ntq-solution.com.vn>
2020-05-12 23:13:22 +0700

    updat code

--- src/layout/Main/index.js
+++ src/layout/Main/index.js
@@ -27,17 +27,7 @@ class Main extends PureComponent {
         <div className="content-wrapper">
           <section class="content-header">
             <div class="container-fluid">
-              <div class="row mb-2">
-                <div class="col-sm-6">
-                  <h1>Legacy User Menu</h1>
-                </div>
-                <div class="col-sm-6">
-                  <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Legacy User Menu</li>
-                  </ol>
-                </div>
-              </div>
+              {children}
             </div>
           </section>
         </div>
